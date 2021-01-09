d955dac7b0a284b080d8f38b0feebbbc3310fd22
daadestroyer <nigamshubham2000@gmail.com>
2021-01-09 17:33:22 +0530

    Get user by id API created successfully

--- src/main/java/com/daadestroyer/LibraryManangementBackend/Controller/UserController.java
+++ src/main/java/com/daadestroyer/LibraryManangementBackend/Controller/UserController.java
@@ -1,10 +1,14 @@
 package com.daadestroyer.LibraryManangementBackend.Controller;
 
 import com.daadestroyer.LibraryManangementBackend.Entity.User;
+import com.daadestroyer.LibraryManangementBackend.Exception.UserNotFoundException;
 import com.daadestroyer.LibraryManangementBackend.Repository.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 public class UserController {
@@ -18,6 +22,18 @@ public class UserController {
     public String addUser(@RequestBody User user) {
         this.userRepo.save(user);
         return "User Created";
+    }
+
+    // get-user API
+    @GetMapping("/get-user/{user_id}")
+    public ResponseEntity<?> getUser(@PathVariable int user_id){
+
+        Optional<User> user = this.userRepo.findById(user_id);
+        if(user.isPresent()){
+            return new ResponseEntity<>(user, HttpStatus.FOUND);
+        }else {
+            return new ResponseEntity<>("User Not Found...", HttpStatus.NOT_FOUND);
+        }
     }
 
 
