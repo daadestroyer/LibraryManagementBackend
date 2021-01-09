01f00f90d8564b6e23e2e33342d2f16aafe842d1
Shubhanshu Arya <shubhanshuarya2020@gmail.com>
2021-01-09 19:22:40 +0530

    get-book by Id API Created Successfully

--- src/main/java/com/daadestroyer/LibraryManangementBackend/Controller/BookController.java
+++ src/main/java/com/daadestroyer/LibraryManangementBackend/Controller/BookController.java
@@ -4,10 +4,10 @@ import com.daadestroyer.LibraryManangementBackend.Entity.Book;
 import com.daadestroyer.LibraryManangementBackend.Repository.BookRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 public class BookController {
@@ -15,10 +15,22 @@ public class BookController {
     @Autowired
     private BookRepo bookRepo;
 
+    // add-book API
     @PostMapping("/add-book")
     @ResponseStatus(HttpStatus.CREATED)
     public String addBook(@RequestBody Book book){
             this.bookRepo.save(book);
             return "Book Created";
+    }
+
+    // get-book by Id API
+    @GetMapping("/get-book/{id}")
+    public ResponseEntity<?> getBookById(@PathVariable int id){
+        Optional<Book> book = this.bookRepo.findById(id);
+        if(book.isPresent()){
+            return new ResponseEntity<>(book, HttpStatus.FOUND);
+        }else{
+            return new ResponseEntity<>("Book Not Found...", HttpStatus.NOT_FOUND);
+        }
     }
 }
