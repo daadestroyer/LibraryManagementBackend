18ec2fe5d0872a75682df059d128393450eb8cea
daadestroyer <nigamshubham2000@gmail.com>
2021-01-09 12:26:48 +0530

    Project Structure Created....

---
+++ src/main/java/com/daadestroyer/LibraryManangementBackend/LibraryManangementBackendApplication.java
@@ -0,0 +1,13 @@
+package com.daadestroyer.LibraryManangementBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManangementBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryManangementBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/daadestroyer/LibraryManangementBackend/LibraryManangementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daadestroyer.LibraryManangementBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManangementBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
